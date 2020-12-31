6f3366e97020df30a815149c240311c6de46c6a8
DaeSeok_Song <zxdsaqwe1231@naver.com>
2020-12-31 18:19:29 +0900

    Firebase(Cloud FireStore)에 앱 연동 설정 완료

---
+++ app/src/main/java/Model/User.java
@@ -0,0 +1,31 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class User {
+
+    // 사용자의 ID, Password
+    private String id, pwd;
+    // 사용자의 관심종목
+    private ArrayList<String> interestedStocks = new ArrayList<>();
+
+    public User() {
+
+    }
+
+    public void signUp() {
+
+    }
+
+    public void addInterestedStock(String name) {
+
+    }
+
+    public void subInterestedStock(String name) {
+
+    }
+
+    public void readInterestedStock() {
+
+    }
+}
