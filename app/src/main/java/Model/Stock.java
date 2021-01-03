36343be0c2cb45a008bc6b29dd652bbc8920e614
DaeSeok_Song <zxdsaqwe1231@naver.com>
2021-01-03 19:17:35 +0900

    1. ykm989의 안드로이드 스튜디오 에러 해결을 위한 app:gradle 수정

--- app/src/main/java/Model/Stock.java
+++ app/src/main/java/Model/Stock.java
@@ -1,15 +1,6 @@
 package Model;
 
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.Serializable;
