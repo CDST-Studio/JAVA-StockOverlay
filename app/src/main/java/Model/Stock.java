4cad69a4daac042ebae3de834fa8d13d5507eafc
bbaesi <bbaeji013@naver.com>
2021-01-05 18:25:27 +0900

    1. Login 액티비티.xml 작성중 70$ 2. Login.java 작성중

--- app/src/main/java/Model/Stock.java
+++ app/src/main/java/Model/Stock.java
@@ -1,45 +1,22 @@
 package Model;
 
-import android.util.Log;
-
-import androidx.annotation.Nullable;
-
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.io.Serializable;
 
 public class Stock implements Serializable {
     // FireStore(Firebase) 접속용 Instance
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private final DocumentReference docRef = db.collection("Stock").document("StockNameToCode");
 
     private String name; // 종목명
-    private String stockCode; // 종목코드
-    private String detailCode; // 업종코드
+    private int stockCode; // 종목코드
+    private int price; // 현재가
 
     public Stock(String name) {
         this.name = name;
 
-        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot snapshot, @Nullable FirebaseFirestoreException e) {
-                if (e != null) {
-                    Log.w("Failed", "Listen failed.", e);
-                    return;
-                }
-
-                if (snapshot != null && snapshot.exists()) {
-                    stockCode = snapshot.getData().get(name).toString();
-                    Log.d("Stock name To code", "StockCode: " + stockCode);
-                } else {
-                    Log.d("Null", "Current data: null");
-                }
-            }
-        });
+        DocumentReference docRef = db.collection("Stock").document("StockNameToCode");
     }
 
     // 해당 종목의 현재가 메서드
--- app/src/main/java/View/login.java
+++ app/src/main/java/View/login.java
@@ -1,5 +1,12 @@
 package View;
 
-public class login {
-    //test1229
-}
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        }}
