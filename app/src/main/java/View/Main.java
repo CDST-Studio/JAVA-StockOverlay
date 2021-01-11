638612fdc1f65b1a2e2e8290baa8e93978d07096
bbaesi <bbaeji013@naver.com>
2021-01-11 21:37:11 +0900

    1. 깃허브 최신버전 로드 후 일단 돌아가는 코드만 다시 붙여넣음 2. Login에 몇가지 버튼 이벤트 구현 했는데 only 구글 로그인으로 변경한다면 차후에 수정함 3. Main에 액티비티 전환 및 몇가지 버튼 구현중 40% 4. Main에 데이터 바인딩 실험중인데 이렇게 쓰는게 맞는지 경호 확인 바람.

---
+++ app/src/main/java/View/Main.java
@@ -0,0 +1,75 @@
+package View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.needfor.stockoverlay.MainActivity;
+import com.needfor.stockoverlay.R;
+import com.needfor.stockoverlay.databinding.ActivityMainBinding;
+
+public class Main  extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater()); //데이터 바인딩
+        //createStock();
+        setContentView(R.layout.activity_main);
+
+        Button Button_search = binding.ButtonSearch;
+        Button_search.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                callSearchStockActivity();
+            }
+        });
+
+        Button Button_Bookmark = binding.ButtonBookmark;
+        Button_Bookmark.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                callBookmarkActivity();
+            }
+        });
+
+
+
+    }
+    private void callSearchStockActivity(){ //main에서 상세 검색 창으로 이동
+        Intent search_stockIntent = new Intent(this, Search_Stock.class);
+        startActivity(search_stockIntent);
+    }
+
+    private void callBookmarkActivity(){ //main에서 bookmark로 이동
+        Intent search_BookmarkIntent = new Intent(this, Bookmark.class);
+        startActivity(search_BookmarkIntent);
+    }
+
+    /*private void createStock() { // 주식 레이아웃 동적 생성 크롤링한 Array 출력
+        LinearLayout Layout_stock = new LinearLayout(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        Layout_stock.setLayoutParams(params);
+        Button btn = new Button(this);
+        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams
+                (ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+        btn.setLayoutParams(lp);
+        btn.setText("test");
+        Layout_stock.addView(btn);
+        setContentView(Layout_stock);
+        //TextView textstock = new TextView(getApplication());
+    }
+     */
+
+}
+
+
