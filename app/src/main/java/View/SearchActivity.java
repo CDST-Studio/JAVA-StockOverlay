6edc7e40cb4d9b18e7823edb33ec3fe9e85db920
bbaesi <bbaeji013@naver.com>
2021-01-27 18:40:55 +0900

    1. 서치뷰 여백 클릭해도 반응하도록 수정

--- app/src/main/java/View/SearchActivity.java
+++ app/src/main/java/View/SearchActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -44,49 +45,53 @@ public class SearchActivity extends AppCompatActivity {
         bundle = new Bundle();
 
         SearchView searchView = (SearchView) findViewById(R.id.search_bar);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        searchView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) { //입력결과
-                query = query.trim();
-                query = query.replaceAll("(^\\p{Z}+|\\p{Z}+$)",""); // 문자열 공백 제거
+            public void onClick(View v){
+                searchView.setIconified(false);
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) { //입력결과
+                        query = query.trim();
+                        query = query.replaceAll("(^\\p{Z}+|\\p{Z}+$)",""); // 문자열 공백 제거
 
-               try{ // 검색어가 숫자가 아니면, 대문자로 변환
-                   int checknum = Integer.parseInt(query);
-               }
-               catch (NumberFormatException e) {
-                   query = query.toUpperCase();
-               }
+                        try{ // 검색어가 숫자가 아니면, 대문자로 변환
+                            int checknum = Integer.parseInt(query);
+                        }
+                        catch (NumberFormatException e) {
+                            query = query.toUpperCase();
+                        }
 
-               stock = search.searchStock(assetManager, query);
+                        stock = search.searchStock(assetManager, query);
 
-               if(stock != null && stock.size() > 0) { //검색할 때 마다 프래그먼트 초기화
-                   searchableFragment = new SearchableFragment();
-                   bundle.putParcelableArrayList("stock",stock);
+                        if(stock != null && stock.size() > 0) { //검색할 때 마다 프래그먼트 초기화
+                            searchableFragment = new SearchableFragment();
+                            bundle.putParcelableArrayList("stock",stock);
 
-                   for(int i=0; i<stock.size(); i++) { // SearchableFragment로 값 전달
-                       name= stock.get(i).getName();
-                       code= stock.get(i).getStockCode();
-                       bundle.putString("name"+i, name);
-                       bundle.putString("code"+i, code);
-                       searchableFragment.setArguments(bundle);
-                   }
+                            for(int i=0; i<stock.size(); i++) { // SearchableFragment로 값 전달
+                                name= stock.get(i).getName();
+                                code= stock.get(i).getStockCode();
+                                bundle.putString("name"+i, name);
+                                bundle.putString("code"+i, code);
+                                searchableFragment.setArguments(bundle);
+                            }
 
-                   intent.putExtra("bookmark", name); // ListSearchAdapter 로 값 전달
-                   getSupportFragmentManager().beginTransaction().replace(R.id.search_result, searchableFragment).commitAllowingStateLoss();
-                   Toast.makeText(SearchActivity.this, "검색완료"+stock, Toast.LENGTH_SHORT).show();
-                   return true; }
+                            intent.putExtra("bookmark", name); // ListSearchAdapter 로 값 전달
+                            getSupportFragmentManager().beginTransaction().replace(R.id.search_result, searchableFragment).commitAllowingStateLoss();
+                            Toast.makeText(SearchActivity.this, "검색완료"+stock, Toast.LENGTH_SHORT).show();
+                            return true; }
 
-                Toast.makeText(SearchActivity.this, "검색어를 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
-                return false;
-            }
+                        Toast.makeText(SearchActivity.this, "검색어를 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
 
-            @Override
-            public boolean onQueryTextChange(String newText) { //입력중
-                return false;
+                    @Override
+                    public boolean onQueryTextChange(String newText) { //입력중
+                        return false;
+                    }
+                });
             }
         });
-
-
     }
 
     //  -------------- 앱바(액션바) 및 메뉴 생성 메서드 --------------
