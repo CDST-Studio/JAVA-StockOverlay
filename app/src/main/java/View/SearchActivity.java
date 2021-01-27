c10e04b788833332a4aefc0546304ba26d4247bb
bbaesi <bbaeji013@naver.com>
2021-01-27 20:15:07 +0900

    1. SearchActivity 서치뷰 여백 클릭해도 반응하도록 수정 2. SearchActivity 뒤로가기 버튼 이벤트 구현 3. SearchActivity 서치뷰 Query Hint가 정상출력 안되는거 수정

--- app/src/main/java/View/SearchActivity.java
+++ app/src/main/java/View/SearchActivity.java
@@ -25,7 +25,6 @@ public class SearchActivity extends AppCompatActivity {
 
     private Search search;
     private AssetManager assetManager;
-    private Parsing parsing;
     private SearchableFragment searchableFragment;
     private Intent intent;
     private ArrayList<Stock> stock;
@@ -39,60 +38,56 @@ public class SearchActivity extends AppCompatActivity {
         createToolbar();
 
         assetManager = getResources().getAssets();
-        parsing = new Parsing();
         search = new Search();
         intent = new Intent(SearchActivity.this, ListSearchAdapter.class);
         bundle = new Bundle();
 
         SearchView searchView = (SearchView) findViewById(R.id.search_bar);
-        searchView.setOnClickListener(new View.OnClickListener() {
+        //searchView.setIconified(f   alse);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
             @Override
-            public void onClick(View v){
-                searchView.setIconified(false);
-                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                    @Override
-                    public boolean onQueryTextSubmit(String query) { //입력결과
-                        query = query.trim();
-                        query = query.replaceAll("(^\\p{Z}+|\\p{Z}+$)",""); // 문자열 공백 제거
+            public boolean onQueryTextSubmit(String query) { //입력결과
+                query = query.trim();
+                query = query.replaceAll("(^\\p{Z}+|\\p{Z}+$)",""); // 문자열 공백 제거
 
-                        try{ // 검색어가 숫자가 아니면, 대문자로 변환
-                            int checknum = Integer.parseInt(query);
-                        }
-                        catch (NumberFormatException e) {
-                            query = query.toUpperCase();
-                        }
+                try{ // 검색어가 숫자가 아니면, 대문자로 변환
+                    int checknum = Integer.parseInt(query);
+                }
+                catch (NumberFormatException e) {
+                    query = query.toUpperCase();
+                }
 
-                        stock = search.searchStock(assetManager, query);
+                stock = search.searchStock(assetManager, query);
 
-                        if(stock != null && stock.size() > 0) { //검색할 때 마다 프래그먼트 초기화
-                            searchableFragment = new SearchableFragment();
-                            bundle.putParcelableArrayList("stock",stock);
+                if(stock != null && stock.size() > 0) { //검색할 때 마다 프래그먼트 초기화
+                    searchableFragment = new SearchableFragment();
+                    bundle.putParcelableArrayList("stock",stock);
 
-                            for(int i=0; i<stock.size(); i++) { // SearchableFragment로 값 전달
-                                name= stock.get(i).getName();
-                                code= stock.get(i).getStockCode();
-                                bundle.putString("name"+i, name);
-                                bundle.putString("code"+i, code);
-                                searchableFragment.setArguments(bundle);
-                            }
-
-                            intent.putExtra("bookmark", name); // ListSearchAdapter 로 값 전달
-                            getSupportFragmentManager().beginTransaction().replace(R.id.search_result, searchableFragment).commitAllowingStateLoss();
-                            Toast.makeText(SearchActivity.this, "검색완료"+stock, Toast.LENGTH_SHORT).show();
-                            return true; }
-
-                        Toast.makeText(SearchActivity.this, "검색어를 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
-                        return false;
+                    for(int i=0; i<stock.size(); i++) { // SearchableFragment로 값 전달
+                        name= stock.get(i).getName();
+                        code= stock.get(i).getStockCode();
+                        bundle.putString("name"+i, name);
+                        bundle.putString("code"+i, code);
+                        searchableFragment.setArguments(bundle);
                     }
 
-                    @Override
-                    public boolean onQueryTextChange(String newText) { //입력중
-                        return false;
-                    }
-                });
+                    intent.putExtra("bookmark", name); // ListSearchAdapter 로 값 전달
+                    getSupportFragmentManager().beginTransaction().replace(R.id.search_result, searchableFragment).commitAllowingStateLoss();
+                    Toast.makeText(SearchActivity.this, "검색완료", Toast.LENGTH_SHORT).show();
+                    return true; }
+
+                Toast.makeText(SearchActivity.this, "검색어를 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) { //입력중
+                return false;
             }
         });
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
