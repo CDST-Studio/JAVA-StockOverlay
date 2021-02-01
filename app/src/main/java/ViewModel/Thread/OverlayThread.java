1d69dd64b8b0eb0dce82c95d946d711a428e3a40
DaeSeok_Song <zxdsaqwe1231@naver.com>
2021-02-01 21:57:44 +0900

    1. DBA에 addTargetProfit이랑 subTargetProfit, getTargetProfits 메서드 추가 2. getTargetProfit 이랑 getPurchasePrice 메서드도 추가 3. PurchasePriceDialog 에서 기존에 입력한 매입가나 목표수익 있으면 가져오도록 설정 4. OverlayThread랑 PriceThread에 매입가 대비 얼마나 올랐는지 반영되도록 수정 5. gradle에 notification용 implement하나 추가 6. 목표수익 달성시 Notification 알림 추가

--- app/src/main/java/ViewModel/Thread/OverlayThread.java
+++ app/src/main/java/ViewModel/Thread/OverlayThread.java
@@ -1,5 +1,21 @@
 package ViewModel.Thread;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.needfor.stockoverlay.R;
+import com.needfor.stockoverlay.StartActivity;
+
 import java.util.ArrayList;
 
 import Model.Stock;
@@ -7,6 +23,7 @@ import Module.Crawling;
 import ViewModel.OverlayViewModel;
 
 public class OverlayThread extends OverlayViewModel implements Runnable {
+    private Context context;
     private OverlayViewModel mModel;
     private ArrayList<Stock> tStockList;
 
@@ -22,7 +39,7 @@ public class OverlayThread extends OverlayViewModel implements Runnable {
         }
     }
 
-    private void priceCompare(){
+    private void priceCompare() {
         mModel = new OverlayViewModel();
         tStockList = new ArrayList<Stock>();
         tStockList = mModel.getStockList().getValue();//LiveData Get
@@ -33,17 +50,75 @@ public class OverlayThread extends OverlayViewModel implements Runnable {
         if(mModel.getStockList().getValue() != null) {
             for (int i = 0; i < tStockList.size(); i++) {
                 Crawling crawling = new Crawling(mModel.getStockList().getValue().get(i));
-                //아래줄도 에러의 주 범
-                if(!(tStockList.get(i).getCurrentPrice().equals(crawling.currentPrice()))) {//새 값을 가져와서 현재값 비교
+
+                //새 값을 가져와서 현재값 비교
+                if(!(tStockList.get(i).getCurrentPrice().equals(crawling.currentPrice()))) {
                     tStockList.get(i).setCurrentPrice(crawling.currentPrice());//
                     tStockList.get(i).setChangeRate(crawling.changeRate());
                     tStockList.get(i).setChange(crawling.change());
                     tStockList.get(i).setChangePrice(crawling.changePrice());
+                    tStockList.get(i).setProfitAndLoss();
 
                     changeFlag = 1;
                 }
+
+                // 목표수익 달성여부 확인 후 알림
+                achievedTargetProfit(i);
             }
         }
         if(changeFlag == 1) mModel.getStockList().postValue(tStockList);
     }
+
+    // 목표수익 달성 시 알림해주는 메서드
+    public void achievedTargetProfit(int idx) {
+        Stock targetStock = mModel.getStockList().getValue().get(idx);
+
+        String targetProfit = targetStock.getTargetProfit().replace(",", "");
+        String profitAndLoss = targetStock.getProfitAndLoss().replace(",", "");
+        if(targetProfit.equals(profitAndLoss)) {
+            //Notification용 채널 생성
+            createNotificationChannel(targetStock.getName());
+
+            Intent intent = new Intent(context, StartActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, targetStock.getName())
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.icon))
+                    .setSmallIcon(R.drawable.icon)
+                    .setContentTitle(targetStock.getName())
+                    .setContentText("목표수익 "+targetStock.getTargetProfit()+"원 달성")
+                    .setSubText("증권앱에서 매도해주세요")
+                    .setTicker("흑우상향, "+targetStock.getName()+" 목표수익 달성알림")
+                    // 알림시 진동과 소리 설정
+                    .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE)
+                    // 우선순위 설정
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    // Notification 알림 클릭시 반응
+                    .setContentIntent(pendingIntent) // Intent 실행(특정 Actitivy 실행)
+                    .setAutoCancel(true); // 클릭시 해당 Notification 삭제
+
+            // 알림 표시
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+            notificationManager.notify(idx, builder.build());
+        }
+    }
+
+    // Notification 알림용 채널 생성 메서드
+    private void createNotificationChannel(String stockName) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = context.getString(R.string.app_name);
+
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(stockName, name, importance);
+            channel.setDescription(stockName+"의 Notification 채널");
+
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void setContext(Context context) { this.context = context; }
 }
