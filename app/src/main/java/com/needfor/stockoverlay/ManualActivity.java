75cc52db73eef9bb1e09a8f23f450f4128c384c2
Huko <ykm989@naver.com>
2021-02-01 22:27:02 +0900

    push 전 commit 설명서 추가

---
+++ app/src/main/java/com/needfor/stockoverlay/ManualActivity.java
@@ -0,0 +1,15 @@
+package com.needfor.stockoverlay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManualActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual);
+    }
+
+}
