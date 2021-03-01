627e118d9f9ee6945180d41d7ed4a068cfcca42d
ykm989 <ykm989@naver.com>
2021-03-01 15:42:30 +0900

    CaculatorActivity.java 생성 및 기초 메소드 작업

---
+++ app/src/main/java/View/CaculatorActivity.java
@@ -0,0 +1,45 @@
+package View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cdst.stockoverlay.R;
+
+public class CaculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caculator);
+
+        
+    }
+
+    private double caculBuy(String name, int amount, int stockPrice){
+        return (amount * stockPrice) + caculCommission(name, amount, stockPrice) + ((amount * stockPrice) * 0.0043);//매수금액 + 수수료 + 유관기관제비용
+    }
+    private double caculSell(String name, int amount, int stockPrice){
+        return (amount * stockPrice) - caculCommission(name, amount, stockPrice) - ((amount * stockPrice) * 0.0043) - ((amount * stockPrice) * 0.0021);//매도금액 - 수수료 - 유관기관제비용 - 세금
+    }
+    private double caculCommission(String name, int amount, int stockPrice){
+        return amount * stockPrice * stockCommission(name);
+    }
+
+
+    private double stockCommission(String name){
+        switch (name){
+            case "나무증권":
+                return 0.01;
+            case "영웅문":
+                return 0.015;
+            case "한국투자증권":
+                return 0.0140527;
+            case "신한투자증권":
+                return 0.1891639;
+            default:
+                return 0.015;
+        }
+
+    }
+}
