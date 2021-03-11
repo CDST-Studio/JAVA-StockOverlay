9a8edd3f209acbb28ee94687217b53263431232d
ykm989 <ykm989@naver.com>
2021-03-12 00:11:31 +0900

    CalculatorActivity 리사이클러뷰 구현

---
+++ app/src/main/java/View/Adapter/CalculAdapter.java
@@ -0,0 +1,114 @@
+package View.Adapter;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cdst.stockoverlay.R;
+
+import java.util.ArrayList;
+
+public class CalculAdapter extends RecyclerView.Adapter<CalculAdapter.ViewHolder>
+{
+    private ArrayList<String> listData = new ArrayList<>();
+
+    private SparseBooleanArray selected = new SparseBooleanArray();//아이템 클릭 상태를 저장할 array 객체
+    private int prePosition = -1;
+    private Context context;
+
+
+
+    public CalculAdapter(ArrayList<String> list){
+        this.listData = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.custom_list_item4, parent, false);
+        CalculAdapter.ViewHolder vh = new CalculAdapter.ViewHolder(view);
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        /*
+        if (position == 0){
+            holder.textView.setText("총 매수");
+        }
+        else if (position == 1){
+            holder.textView.setText("총 매도");
+        }
+        else{
+            holder.textView.setText("총 손익");
+        }
+         */
+        if(position == 0) holder.textView.setBackgroundColor(Color.parseColor("#343434"));
+        else if (position == 1) holder.textView.setBackgroundColor(Color.parseColor("#5F5E5D"));
+        else holder.textView.setBackgroundColor(Color.parseColor("#808080"));
+
+        holder.onBind(listData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    public void addItem(String str){
+        listData.add(str);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView textView;
+
+        ViewHolder(View view){
+            super(view);
+
+            view.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+            textView = view.findViewById(R.id.cal_name);
+        }
+
+
+        void onBind(String str){
+            textView.setText(str);
+        }
+
+        private void changeVisibility(final boolean isExpanded){
+            int dpValue = 150;
+            float d = context.getResources().getDisplayMetrics().density;
+            int height = (int) (dpValue + d);
+
+            ValueAnimator va = isExpanded ? ValueAnimator.ofInt(0, height) : ValueAnimator.ofInt(height, 0);
+            va.setDuration(600);
+            va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    int value = (int) animation.getAnimatedValue();
+
+
+                }
+            });
+        }
+
+    }
+
+}
--- app/src/main/java/View/Fragment/CalOutputFragment.java
+++ app/src/main/java/View/Fragment/CalOutputFragment.java
@@ -8,17 +8,48 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.cdst.stockoverlay.R;
+
+import java.util.ArrayList;
+
+import View.Adapter.CalculAdapter;
 
 public class CalOutputFragment  extends Fragment {
 
     private View CalOutputview;
+    private RecyclerView recyclerView;
+    private CalculAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> datalist;//혹시나 해서 ㅎㅎ;;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         CalOutputview = (View) inflater.inflate(R.layout.fragment_stockoutput, container,false);
+
+        recyclerView = (RecyclerView) CalOutputview.findViewById(R.id.cal_list);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        init();
+
+        adapter = new CalculAdapter(datalist);
+        recyclerView.setAdapter(adapter);
+
+
         return CalOutputview;
     }
+
+    void init(){
+        datalist = new ArrayList<>();
+        datalist.add("총 매수");
+        datalist.add("총 매도");
+        datalist.add("총 손익");
+    }
+
 }
