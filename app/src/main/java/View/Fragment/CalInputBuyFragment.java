7859f102bcf538e641cd672d419bd868db61247e
Huko <ykm989@naver.com>
2021-03-14 21:36:35 +0900

    노트북 -> 데스크탑 아코디언 구현

--- app/src/main/java/View/Fragment/CalInputBuyFragment.java
+++ app/src/main/java/View/Fragment/CalInputBuyFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,10 +16,20 @@ public class CalInputBuyFragment extends Fragment {
 
     private View CalInputBuyView;
 
+    int price;
+    int quantity;
+    int fee;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         CalInputBuyView = (View) inflater.inflate(R.layout.fragment_stockinput_buy,container,false);
+
+        EditText price_ed = CalInputBuyView.findViewById(R.id.edit_BuyPrice);
+        EditText quantity_ed = CalInputBuyView.findViewById(R.id.edit_BuyQuantity);
+        EditText fee_ed = CalInputBuyView.findViewById(R.id.edit_BuyFee);
+
+        String pricea = price_ed.getText().toString();
 
         return CalInputBuyView;
     }
--- app/src/main/java/View/Fragment/CalOutputFragment.java
+++ app/src/main/java/View/Fragment/CalOutputFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,6 +32,50 @@ public class CalOutputFragment  extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         CalOutputview = (View) inflater.inflate(R.layout.fragment_stockoutput_2, container,false);
 
+        Button buyButton = CalOutputview.findViewById(R.id.avg_Buy_Button);
+        Button sellButton = CalOutputview.findViewById(R.id.avg_Sell_Button);
+        Button totalButton = CalOutputview.findViewById(R.id.Button_Total_ProfitLoss);
+
+        LinearLayout buyLinear = CalOutputview.findViewById(R.id.cal_Buy_Linear);
+        LinearLayout sellLinear = CalOutputview.findViewById(R.id.cal_sell_Linear);
+        LinearLayout totalLinear = CalOutputview.findViewById(R.id.cal_total_Linear);
+
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if(buyLinear.getVisibility() == View.GONE){
+                    buyLinear.setVisibility(View.VISIBLE);
+                }
+                else{
+                    buyLinear.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        sellButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sellLinear.getVisibility() == View.GONE){
+                    sellLinear.setVisibility(View.VISIBLE);
+                }
+                else {
+                    sellLinear.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        totalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(totalLinear.getVisibility() == View.GONE){
+                    totalLinear.setVisibility(View.VISIBLE);
+                }
+                else{
+                    totalLinear.setVisibility(View.GONE);
+                }
+            }
+        });
         //recyclerView = (RecyclerView) CalOutputview.findViewById(R.id.cal_list);
         //recyclerView.setHasFixedSize(true);
 
